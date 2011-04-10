8e4b7ff8515bd06dc44e5a687d7400581563d5dc
Jason Scheirer <jason.scheirer@gmail.com>
2011-04-09 23:21:51 -0700

    Problem2

--- problem1.go
+++ problem1.go
@@ -10,11 +10,14 @@ Find the sum of all the multiples of 3 or 5 below 1000.
 
 package main
 
+import fmt "fmt"
+
 func main() {
-	var total int = 0;
+	total := 0;
 	for i := 1; i < 1000; i++ {
 		if (i % 5 == 0) && (i % 3 == 0) {
 			total += i
 		}
 	}
+	fmt.Printf("%v\n", total)
 }
